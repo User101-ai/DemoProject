7d9aea10f30029cdce46955d9f99878528967a12
Prince Giri <prince.giri@ecifm.com>
2025-10-06 15:08:31 +0530

    Changed Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
+    public static void getMessage(){
+        System.out.println("hello i am getMessage() method!!!");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello Class");
+        getMessage();
     }
 }
